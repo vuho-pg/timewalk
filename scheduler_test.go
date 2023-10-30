48cfffcf0d02ad87a45dd5e1e95dd245ad57524f
vuhk <qwerty22121998@gmail.com>
2023-10-30 18:25:32 +0700

    fix test

--- scheduler_test.go
+++ scheduler_test.go
@@ -33,7 +33,7 @@ func TestSchedule_String(t *testing.T) {
 		Day(At(22)).
 		DayOfWeek(At(time.Tuesday)).
 		Hour(At(3)).Minute(At(11)).Second(At(0))
-	str := fmt.Sprintf("at 2023rd year, at December, at 22nd day, at Tuesday, at 3rd hour, at 11th minute, at 0th second, start from %v, end at %v with 1h0m0s duration", now.In(loc).Format(time.RFC850), now.Add(time.Hour).Format(time.RFC850))
+	str := fmt.Sprintf("at 2023rd year, at December, at 22nd day, at Tuesday, at 3rd hour, at 11th minute, at 0th second, start from %v, end at %v with 1h0m0s duration", now.In(loc).Format(time.RFC850), now.In(loc).Add(time.Hour).Format(time.RFC850))
 	assert.Equal(t, str, s.String())
 }
 

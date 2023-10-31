c38728ca4f271b889d708c074215861765d56c88
vuho <vu.ho@onemount.com>
2023-10-31 14:21:05 +0700

    feat(day-of-week): add day of week support

--- time_field.go
+++ time_field.go
@@ -18,6 +18,33 @@ func (f TField[T]) String(unitName string) string {
 	return b.String()
 }
 
+// PreviousInPool returns the previous value in the pool
+func (f TField[T]) PreviousInPool(data T, pool []T) *T {
+	if len(pool) == 0 {
+		return nil
+	}
+	res := T(-1)
+	for _, v := range pool {
+		if v <= data && f.Match(v) {
+			res = max(res, v)
+		}
+	}
+
+	if res == -1 {
+		return nil
+	}
+	return &res
+}
+
+func (f TField[T]) Match(data T) bool {
+	for _, u := range f {
+		if u.Match(data) {
+			return true
+		}
+	}
+	return false
+}
+
 func (f TField[T]) Previous(data T) *T {
 	var res *T
 	for _, u := range f {
